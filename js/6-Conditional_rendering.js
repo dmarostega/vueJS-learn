5fd05149d642932597d800bc1e96b6d1fc724e40
dmarostega <dmarostega@gmail.com>
2019-04-11 14:40:50 -0300

    6. Conditional Rendering and list rendering,

---
+++ js/6-Conditional_rendering.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * RENDERIZAÇÃO CONDICIONAL
+ *      v-if
+ */
+
+ new Vue({
+     el:"#app",
+     data:{
+         message: "Testando Item 6, da documentação.",
+         awesome:"Yes",
+         ok:"ok",
+         type:'B'
+     }
+ });
+
+
+ 
+ /**    
+  *     Lista de Renderização v-for
+  */
+
+  
+ new Vue({
+     el:"#app-list",
+     data:{
+         parentMessage:'Parent',
+        items:[
+             {message: 'Item 1'},
+             {message: 'Item 2'},
+             {message: 'Last Item'}
+         ],
+         object:{
+             firstName:'John',
+             lastName:'Doe',
+             age:23
+         }
+     }
+ })
